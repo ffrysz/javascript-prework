a1cf851d76309b28848ffde356f77b71a23bf065
Filip Frysz <ffrysz@gmail.com>
2021-04-06 10:16:05 +0200

    Add computer move logic

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
-let computerMove = "kamień";
-let playerMove = "papier";
+let computerMove = "nieznany ruch";
+//let playerMove = "nieznany ruch";
+let randomNumber = Math.floor(Math.random() * 3 + 1);
 
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
-printMessage("Zagrałeś " + playerMove);
+console.log(randomNumber);
+if (randomNumber == 1) {
+    computerMove = "kamień";
+} else if (randomNumber == 2) {
+    computerMove = "papier";
+} else if (randomNumber == 3) {
+    computerMove = "nożyce";
+}
+
+console.log(computerMove);
