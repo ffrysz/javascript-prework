b9d52168a4b22acea305f2647f7cf3bf32b65c6b
Filip Frysz <ffrysz@gmail.com>
2021-04-06 10:22:53 +0200

    Add player-move logic

--- js/script.js
+++ js/script.js
@@ -1,8 +1,20 @@
 let computerMove = "nieznany ruch";
-//let playerMove = "nieznany ruch";
+let playerMove = "nieznany ruch";
 let randomNumber = Math.floor(Math.random() * 3 + 1);
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
+if (playerInput == '1') {
+    playerMove = 'kamień';
+} else if (playerInput == "2") {
+    playerMove = "papier";
+} else if (playerInput == "3") {
+    playerMove = "nożyce";
+}
+
+console.log('Gracz wpisał: ' + playerInput);
+console.log(playerMove);
 console.log(randomNumber);
+
 if (randomNumber == 1) {
     computerMove = "kamień";
 } else if (randomNumber == 2) {
@@ -12,3 +24,6 @@ if (randomNumber == 1) {
 }
 
 console.log(computerMove);
+
+printMessage("Komputer zagrał " + computerMove);
+printMessage("Twój ruch to: " + playerMove);
